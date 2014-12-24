bee06400651096a0b243d913722450fe88c9699a
Thomas Segismont <tsegismo@redhat.com>
2014-12-24 12:35:38 +0100

    vertx options: use specific domain

--- src/main/java/io/vertx/example/jmx/Example.java
+++ src/main/java/io/vertx/example/jmx/Example.java
@@ -5,17 +5,17 @@ import io.vertx.core.VertxOptions;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
 
-import javax.management.MBeanServer;
-import javax.management.ObjectInstance;
-import java.lang.management.ManagementFactory;
-
 /**
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
 public class Example {
 
   public static void main(String[] args) {
-    Vertx vertx = Vertx.vertx(new VertxOptions().setMetricsEnabled(true).setJmxEnabled(true));
+    VertxOptions options = new VertxOptions()
+      .setMetricsEnabled(true)
+      .setJmxEnabled(true)
+      .setJmxDomain("io.vertx");
+    Vertx vertx = Vertx.vertx(options);
     HttpServer server = vertx.createHttpServer(new HttpServerOptions().setPort(8080));
     server.requestStream().handler(req -> {
       req.response().setChunked(true).write("HELLO WORLD").end();
@@ -23,10 +23,10 @@ public class Example {
     server.listen(s -> {
       if (s.succeeded()) {
         System.out.println("Listening");
-        MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
-        for (ObjectInstance mbean : mbeanServer.queryMBeans(null, null)) {
-          System.out.println("MBean Found " + mbean.getClassName() + " " + mbean.getObjectName().getCanonicalName());
-        }
+//        MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
+//        for (ObjectInstance mbean : mbeanServer.queryMBeans(null, null)) {
+//          System.out.println("MBean Found " + mbean.getClassName() + " " + mbean.getObjectName().getCanonicalName());
+//        }
       } else {
         System.out.println("Failed");
       }
